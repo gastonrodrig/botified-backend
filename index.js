8cdd8347057bfc0250224d77f74fcea959c71112
Gaston Rodriguez <gastonrodriguez@MacBook-Air-de-Gaston.local>
2025-06-23 01:32:00 -0500

    Modificara configuracion de CORS, para permitir origins, methods y headers

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // Rutas
